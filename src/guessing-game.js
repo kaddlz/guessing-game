0d1e62bbcfcd0cda480c7a213c2b577dc7ff0430
kaddlz <roslanov@icloud.com>
2023-10-30 01:42:51 +0300

    feat: complete guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.mid = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.mid = Math.round((min + max) / 2);
     }
 
     guess() {
-
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
+        this.mid = Math.round((this.min + this.max) / 2);
     }
 
     greater() {
-
+        this.min = this.mid;
+        this.mid = Math.round((this.min + this.max) / 2);
     }
 }
+
 
 module.exports = GuessingGame;
